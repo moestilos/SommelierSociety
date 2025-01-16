4f21ce8a5b4566dc1a1bf27ed95845bc09ec208a
Guillermo Mateos <gmateosoficial@gmail.com>
2025-01-16 13:14:50 +0100

    añado varias cosas

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -13,18 +13,18 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Asegúrate de compilar estilos si usas Tailwind -->
 </head>
-<body class="flex flex-col min-h-screen">
+<body class="flex flex-col min-h-screen" style="background-image: url('{{ asset('img/fondocursos.jpg') }}'); background-size: cover; background-position: center;">
         <!-- ENCABEZADO DE LA PÁGINA -->
   <header class="text-gray-400 bg-black p-2 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
               <!-- Logo VinoA -->
-              <img src="{{ asset('img/logoVinoA.png') }}" alt="Logo VinoA" class="w-10 h-10 rounded-full mr-3">
+              <img src="{{ asset('img/logonuevo.jpeg  ') }}" alt="Logo VinoA" class="w-10 h-10 rounded-full mr-3">
               <span class="ml-3 text-xl">South Wine Academy</span>
           </a>
           <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-              <a href="{{ url('/login') }}" class="mr-5 hover:text-yellow">Login</a>
-              <a href="" class="mr-5 hover:text-yellow">Contact</a>
+              <a href="{{ url('/contacto') }}" class="mr-5 hover:text-yellow">Contact</a>
+              <a href="{{ url('/register') }}" class="mr-5 hover:text-yellow">Register</a>
               <a href="" class="mr-5 hover:text-yellow">About Us</a>
               <a href="" class="mr-5 hover:text-yellow">Store</a>
           </nav>
@@ -99,28 +99,11 @@
     </div>
   </section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  <!-- PIE DE LA PÁGINA -->
   <footer class="text-gray-400 bg-black p-4 body-font">
     <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-      <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span class="ml-3 text-xl">South Wine Academy</span>
+      <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+        <img src="{{ asset('img/logonuevo.jpeg  ') }}" alt="Logo VinoA" class="w-10 h-10 rounded-full mr-3">
       </a>
       <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2025 South Wine Academy —
         <a href="https://github.com/moestilos/SommelierSociety.git" class="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">github</a>
