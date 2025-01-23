10129f13ce884740f69102997f357e9e6aa27781
Ismael Piña <ismael199904@gmail.com>
2025-01-23 12:59:59 +0100

    -1 bloque de tarifa y centrado del mismo

--- resources/views/tarifas.blade.php
+++ resources/views/tarifas.blade.php
@@ -23,14 +23,14 @@
     <!-- Asegúrate de compilar estilos si usas Tailwind -->
 </head>
 
-<body class="flex flex-col min-h-screen"
+<body class="flex flex-col min-h-screen items-center justify-center"
     style="background-image: url('{{ asset('img/fondocursos.jpg') }}'); background-size: cover; background-position: center;">
 
     <!-- component -->
     <section class="py-8">
-    <div class="container px-6 py-8 mx-auto">
+        <div class="container px-6 py-8 mx-auto text-center">
        
-        <div class="grid gap-6 mt-16 -mx-6 sm:gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+        <div class="grid gap-6 mt-16 -mx-6 sm:gap-8 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 justify-center">
             <div class="h-full bg-gray-800 bg-opacity-60 px-4 pt-8 pb-12 rounded-lg overflow-hidden text-center relative transition-transform transform hover:scale-105 hover:shadow-lg">
                 <p class="text-lg font-medium text-gray-100">Intro</p>
                 <h4 class="mt-2 text-4xl font-semibold text-white">19€ <span class="text-base font-normal text-gray-400">/ Month</span></h4>
@@ -183,58 +183,6 @@
                 </div>
  
                 <button onclick="window.location.href='{{ url('/tarifaschek?plan=popular') }}'" class="w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-black p-4 rounded-md hover:bg-yellow-500 focus:outline-none focus:bg-yellow-600">
-                    Choose plan
-                </button>
-            </div>
- 
-            <div class="h-full bg-gray-800 bg-opacity-60 px-4 pt-8 pb-12 rounded-lg overflow-hidden text-center relative transition-transform transform hover:scale-105 hover:shadow-lg">
-                <p class="text-lg font-medium text-gray-100">Exterprise</p>
-                <h4 class="mt-2 text-4xl font-semibold text-white">199€ <span class="text-base font-normal text-gray-400">/ Month</span></h4>
-                <p class="mt-4 text-gray-300">For most businesses that want to optimaize web queries.</p>
- 
-                <div class="mt-8 space-y-8">
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
- 
-                        <span class="mx-4 text-gray-300">All limited links</span>
-                    </div>
- 
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
- 
-                        <span class="mx-4 text-gray-300">Own analytics platform</span>
-                    </div>
- 
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
- 
-                        <span class="mx-4 text-gray-300">Chat support</span>
-                    </div>
- 
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
- 
-                        <span class="mx-4 text-gray-300">Optimize hashtags</span>
-                    </div>
- 
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
- 
-                        <span class="mx-4 text-gray-300">Unlimited users</span>
-                    </div>
-                </div>
- 
-                <button onclick="window.location.href='{{ url('/tarifaschek?plan=exterprise') }}'" class="w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-yellow-500 rounded-md hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600">
                     Choose plan
                 </button>
             </div>
