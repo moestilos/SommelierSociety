4eed1ff5b0838903f1c7dc4100977570f6ebbedd
Ismael Piña <ismael199904@gmail.com>
2025-01-23 11:21:54 +0100

    Enlace a tarifas desde Sommelier

--- resources/views/catasdevino.blade.php
+++ resources/views/catasdevino.blade.php
@@ -18,7 +18,7 @@
 
         @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Dancing+Script:wght@400..700&family=Jaro:opsz@6..72&family=Sofadi+One&family=Teko:wght@300..700&display=swap');
     </style>
-    <title>Vinos</title>
+    <title>Catas de Vino</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Asegúrate de compilar estilos si usas Tailwind -->
 </head>
@@ -26,7 +26,45 @@
 <body class="flex flex-col min-h-screen"
     style="background-image: url('{{ asset('img/fondocursos.jpg') }}'); background-size: cover; background-position: center;">
 
+<section
+  class="relative bg-[url('{{ asset('img/fondocursos.jpg') }}')] bg-cover bg-center bg-no-repeat"
+>
+  <div
+    class="absolute inset-0 bg-gray-900/75 sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+  ></div>
 
+  <div
+    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-10 lg:flex lg:h-screen lg:items-center lg:px-16"
+  >
+    <div class="max-w-xl text-center mx-auto">
+      <h1 class="text-3xl font-extrabold text-white sm:text-5xl">
+        Catas de
+
+        <strong class="block font-extrabold text-rose-500"> Vino. </strong>
+      </h1>
+
+      <p class="mt-4 max-w-lg text-white sm:text-xl/relaxed mx-auto">
+        Inscríbete en nuestras catas de vino y disfruta de una experiencia única.
+      </p>
+
+      <div class="mt-8 flex flex-wrap gap-4 justify-center">
+        <a
+          href="{{ url('/tarifas') }}"
+          class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+        >
+          Get Started
+        </a>
+
+        <a
+          href="#"
+          class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+        >
+          Learn More
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
 
 </body>
 </html>
