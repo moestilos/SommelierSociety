d781d4eac7bf004ed0b2a1d2c707b0fa41eec909
Guillermo Mateos <guillermo.moestilos@gmail.com>
2025-02-04 10:10:25 +0100

    quitado lo de app
    wwrgwrh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,34 +41,7 @@
 
 
 
-        <!-- Sección Reseñas - Animación desde la izquierda -->
-        <div class="container px-5 py-12 mx-auto"
-             data-aos="fade-left"
-             data-aos-offset="200"
-             data-aos-delay="150"
-             data-aos-duration="800">
-            <div class="text-left">
-                @if(request()->routeIs('dashboard'))
-                    <img src="{{ asset('/img/vino1.jpg') }}" 
-                         data-aos="zoom-in-right"
-                         data-aos-delay="300"
-                         class="mx-auto mb-4 rounded-xl shadow-lg w-1/2">
-                @endif
-                <p class="text-dark font-garamond mb-4"
-                   data-aos="fade-right"
-                   data-aos-delay="450">"El vino de Jerez es más que una bebida: es el alma de una tierra bañada por el sol y acariciada por la brisa del Atlántico. Cada copa cuenta una historia de tradición, paciencia y maestría, donde el tiempo y la crianza dan forma a sabores únicos que solo esta tierra puede ofrecer.
-
-                   En Sommelier Society, nos apasiona compartir contigo la riqueza de nuestros vinos, desde la intensidad de un Oloroso hasta la sutileza de un Fino, pasando por el dulzor envolvente de un Pedro Ximénez. Cada sorbo es un viaje al corazón del Marco de Jerez, donde la albariza nutre las viñas y la solera guarda secretos centenarios.
-                   
-                   Gracias por acompañarnos en este recorrido sensorial. Que cada copa que degustes sea un homenaje a la tradición, a los momentos inolvidables y a la magia de Jerez.
-                   
-                   ¡Salud y hasta pronto!"</p>
-                <a href="{{ url('/resenas') }}" 
-                   data-aos="fade-up"
-                   data-aos-delay="600"
-                   class="text-yellow-400 hover:text-yellow-500 font-medium"></a>
-            </div>
-        </div>
+      
     </main>
     
     <footer class="text-gray-400 wine-gradient py-2 body-font">
